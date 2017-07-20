090100afb88241811e0085ed7bb422bf9a740fa6
Garrett-Thomas <league@WTS10.attlocal.net>
2017-07-20 11:54:00 -0700

    Summer workshop day 4

---
+++ src/level0jessica/ForLoopGuantlet.java
@@ -0,0 +1,105 @@
+package level0jessica;
+
+public class ForLoopGuantlet {
+	public static void main(String[] args) {
+		
+			//for (int i = 0; i < 101; i++){
+				//System.out.println(i);
+			//}
+			//for (int i = 100; i >-1; i--){
+				//System.out.println(i);
+			//}
+		//if(4%2==0){
+			//for (int i = 0; i <51; i++){	
+		//System.out.println(i*2);
+			//}
+		//}
+		//for(int i = 1; i<100; i ++){
+			//if(i%2==1){
+			//System.out.println(i);
+			//}
+		//}
+		//for(int i = 0; i<501;i ++){
+		//System.out.println(i);
+		//if(i%2==1){
+			//System.out.println("odd");
+		//}
+		//if(i%2==0){
+			//System.out.println("even");
+		//}
+		//}
+		//for(int i= 0; i<778;i++){
+			
+			//if(i%7==0){
+		//System.out.println(i);
+			//}
+		//}
+		//for(int i=2003; i<2018; i ++){
+			//System.out.println(i);
+			//if(i==2003){
+			//.out.println("I was 0 years old");
+			//}
+			//if(i==2004){
+				//System.out.println("I was 1 year old");	
+			//}
+			//if(i==2005){
+				//System.out.println("I was 2 years old");	
+			//}
+			//if(i==2006){
+				//System.out.println("I was 3 years old");	
+			//}
+			//if(i==2007){
+				//System.out.println("I was 4 years old");	
+			//}
+			//if(i==2008){
+				//System.out.println("I was 5 years old");	
+			//}
+			//if(i==2009){
+				//System.out.println("I was 6 years old");	
+			//}
+			//if(i==2010){
+				//System.out.println("I was 7 years old");	
+			//}
+			//if(i==2011){
+				//System.out.println("I was 8 years old");	
+			//}
+			//if(i==2012){
+				//System.out.println("I was 9 years old");	
+			//}
+			//if(i==2013){
+				//System.out.println("I was 10 years old");	
+			//}
+			//if(i==2014){
+				//System.out.println("I was 11 years old");	
+			//}
+			//if(i==2015){
+				//System.out.println("I was 12 years old");	
+			//}
+			//if(i==2016){
+				//System.out.println("I was 13 years old");	
+			//}
+			//if(i==2017){
+				//System.out.println("I am 14 years old");	
+			//}
+
+
+		//}
+		//System.out.println(00);
+		//.out.println(01);
+		//System.out.println(02);
+		//System.out.println(10);
+		//System.out.println(11);
+		//System.out.println(12);
+		//System.out.println(20);
+		//System.out.println(21);
+		//System.out.println(22);
+		System.out.println(123);
+		System.out.println(456);
+		System.out.println(789);
+		}
+		}
+		
+		
+		
+		
+
